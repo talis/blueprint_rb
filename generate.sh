ce84b822cbf6ed65470ef0abf71abf0353f50413
Ben Paddock <pads@thisispads.me.uk>
2016-05-02 09:30:47 +0100

    Use custom templates for code generation

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 wget http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.1.6/swagger-codegen-cli-2.1.6.jar -O swagger-codegen-cli.jar
-java -jar swagger-codegen-cli.jar generate -l ruby -o . -i https://staging-hierarchy.talis.com/api-docs -c config.json
+java -jar swagger-codegen-cli.jar generate -l ruby -o . -i https://staging-hierarchy.talis.com/api-docs -c config.json -t templates
 
